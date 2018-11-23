8282d936fc59a77d57954fbd98a2898c6d74254d
John <johntu@kth.se>
2018-11-23 21:09:27 +0100

    Ball with touches

---
+++ input.js
@@ -0,0 +1,73 @@
+
+//////////////////////
+// INPUT
+
+var callback = () => {
+  var c = f => c._cbs.push(f);
+  c._cbs = [];
+  c.emit = (...args) => {
+    c._cbs.forEach(f => f.apply(this, args));
+  };
+  return c;
+};
+
+var evtToPos = evt => {
+  return {
+    x: evt.pageX,
+    y: evt.pageY,
+  };
+};
+
+var getRelation = (a, b) => {
+  var dx = b.x-a.x;
+  var dy = b.y-a.y;
+  var h = Math.sqrt(dx*dx+dy*dy);
+  return {
+    dx: dx,
+    dy: dy,
+    h: h,
+  };
+};
+
+var input = {
+  cur: null,
+  prev: null,
+  onTouchStart: callback(),
+  onTouchMove: callback(),
+  onTouchEnd: callback(),
+  onSwipeUp: callback(),
+  onSwipeUpRight: callback(),
+  onSwipeUpLeft: callback(),
+};
+
+var el = document.getElementById("container");
+
+console.log(window.document.body.ontouchstart)
+
+el.addEventListener("touchstart", (evt) => {
+  if (evt.touches.length > 1) return;
+  input.prev = evtToPos(evt.touches[0]);
+  input.onTouchStart.emit(input.prev);
+});
+
+el.addEventListener("touchmove", (evt) => {
+  if (evt.touches.length < 1) return;
+  input.cur = evtToPos(evt.touches[0]);
+  input.onTouchMove.emit(input.cur);
+});
+
+el.addEventListener("touchend", (evt) => {
+  if (!input.cur) return;
+  var r = getRelation(input.prev, input.cur);
+  var v = Math.atan(r.dy/r.dx);
+  if (r.dy < 0) {
+    if (r.h < 30) return;
+    if (v > 0 && v < 1) input.onSwipeUpLeft.emit();
+    else if (v < 0 && v > -1) input.onSwipeUpRight.emit();
+    else input.onSwipeUp.emit();
+  }
+  input.onTouchEnd.emit(input.cur);
+  
+  input.prev = null;
+  input.cur = null;
+});
--- main.js
+++ main.js
@@ -1,8 +1,21 @@
 document.write("hello");
 
 var t = 0;
+var debug = "";
+
+var ballEl = document.getElementById("ball");
+
+function setBallPos(x,y) {
+  ballEl.style.transform = "translate("+x+"px,"+(window.innerHeight/2)+"px)";
+}
 
 timer(update, fixedUpdate);
+
+input.onTouchMove(p => {
+  debug = p.x;
+  t = p.x/5;
+  setBallPos(p.x,p.y);
+})
 
 var obj = {
   numberOfSteps: 12,
@@ -10,12 +23,13 @@ var obj = {
 }
 
 function update(d) {
-  t += d*10;
+  // t += d*10;
   var s = "rgb(0,0,"+Math.floor(t)+")";
   document.body.style.background = s;
   document.body.style.color ="white";
-  document.body.innerHTML = "";
-  document.write(s);
+  //document.body.innerHTML = "";
+  //document.write(debug);
+  //document.write(s);
 }
 
 function fixedUpdate(d) {
